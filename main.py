353b3f72d872ebaeb4e38ab4460f4c657df461d2
Humanbluray <vancarlosfr@gmail.com>
2025-01-24 01:39:56 +0100

    add requirements and procfile

--- main.py
+++ main.py
@@ -57,4 +57,4 @@ def main(page: ft.Page):
 
 if __name__ == '__main__':
     port = int(os.getenv("PORT", 8080))
-    ft.app(target=main, port=port,)  # , view=ft.AppView.WEB_BROWSER)
+    ft.app(target=main, port=port, view=ft.AppView.WEB_BROWSER)  # , view=ft.AppView.WEB_BROWSER)
